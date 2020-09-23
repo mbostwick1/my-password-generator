ad78124cfffefdffad8a21f6d02c45815e610afb
Melanie Bostwick <mbostwick1@gmail.com>
2020-09-22 22:57:08 -0400

    redo. code in wrong function. established variables

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -10,45 +8,36 @@ function writePassword() {
 
   passwordText.value = password;
 
-    //Password Length Prompt//
-    var passwordLengthCon = prompt("Enter a length of at least 8 characters and no more than 128 characters"); 
-  
-    if (passwordLengthCon < 8 || passwordLengthCon > 128){
-      alert("Your password must be at least 8 characters and no more than 128 characters!")
-      }
-    //Once established valid length, get different variable confirmations//
-    else {
-      var lowerCaseCon = confirm("Do you want to use lowercase letters?");
-      var upperCaseCon = confirm("Do you want to use uppercase letters?");
-      var numbersCon = confirm("Do you want to use numbers?");
-      var specialCharactersCon = confirm("Do you want to use special characters?")
-  
-      }
-      //The user must select AT LEAST 1 form of variable for password//
-  
-    if (lowerCaseCon === false && upperCaseCon === false && numbersCon === false && specialCharactersCon === false){
-      alert("You must at least select one character type!")
-      }
-     else {
-       console.log("go to Password function!");
-        generatePassword() ;
-     }
-  
 }
 
 function generatePassword() {
 
-var password = "";
-var passwordLength = "";
+  // Variables for all characters options to generate in password//
+  var passwordLength = 0;
 
-  return "This will be replaced by my generated password"
+  var lowerCaseOpt = "abcdefghijklmnopqrstuvwxyz";
+  var upperCaseOpt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbersOpt = "0123456789";
+  var specialCharOpt = "!@#$%^&*()<>/{}[]+=-_~";
+  var passwordSet = "";
+  var passwordLength = 0;
+
+  // Variables for user prompts/confirms //
+  var lowerCaseCon;
+  var upperCaseCon;
+  var numbersCon;
+  var specialCharCon;
+
+  // Variables to confirm at least one choice has been selected //
+  var characterCon = false;
+  var passwordCon = "";
+
+
+  return "This will be replaced by my generated password";
 }
+
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
 
